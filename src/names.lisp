ae994c6211f09ee5e52c3592ace075d7a99ac08a
digikar99 <shubhamkarayare@gmail.com>
2022-03-17 11:23:39 +0530

    [fix] avoid satisfies type in function-name

--- src/names.lisp
+++ src/names.lisp
@@ -1,12 +1,7 @@
 (in-package :trivial-types)
 
-(defun setf-function-name-p (object)
-  (and (listp object)
-       (null (cddr object))
-       (eq 'setf (car object))
-       (symbolp (cadr object))))
-
 (deftype function-name ()
-  ;; Doesn't work great with subtypep
   "Ref: http://www.lispworks.com/documentation/HyperSpec/Body/26_glo_f.htm#function_name"
-  `(or symbol (satisfies setf-function-name-p)))
+  `(or symbol
+       (cons (eql cl:setf)
+             (cons symbol null))))
